0390d1e228937149a2e359bbef76584aee64b87d
Cengiz YILMAZ <terabilgi@gmail.com>
2023-12-03 17:21:54 +0300

    Template parçalandı

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return render(req, 'pages/index.html')
+
+def about(req):
+    return render(req, 'pages/about.html')
